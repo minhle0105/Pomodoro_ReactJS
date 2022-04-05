12f940d958d530ae1894ccfd6965a34871a520c1
minhle0105 <minh.anh.le.cnn@gmail.com>
2022-04-05 01:57:23 -0400

    Basically working

--- src/App.js
+++ src/App.js
@@ -3,23 +3,34 @@ import {useEffect, useRef, useState} from "react";
 
 function App() {
     const [isOn, setIsOn] = useState(false)
-    const [minute, setMinute] = useState(0);
-    const [second, setSecond] = useState(60);
+    const [minute, setMinute] = useState(25);
+    const [second, setSecond] = useState(0);
     const [finished, setFinished] = useState(false);
+    const [audio, setAudio] = useState(new Audio('/DiVeNha.m4a'))
+    const [breakTitle, setBreakTitle] = useState('');
+
+    const breakTimeTitle = 'BREAK TIME';
+    const workingTimeTitle = 'WORKING TIME';
 
     const intervalId1 = useRef(1);
 
-    let audio = new Audio('/DiVeNha.m4a');
-
-    const playAlarm = () => {
-        audio.play().then(r => console.log(r));
-    }
 
     useEffect(() => {
         if (isOn) {
             intervalId1.current = setInterval(() => {
-                setSecond(prevState => prevState === 1 ? 60 : prevState - 1);
-            }, 100);
+                setSecond(prevState => {
+                    if (prevState === 1) {
+                        return 60;
+                    }
+                    else if (prevState === 0) {
+                        setMinute(prevState => prevState - 1);
+                        return 59;
+                    }
+                    else {
+                        return prevState - 1;
+                    }
+                } );
+            }, 1000);
         }
     }, [isOn])
 
@@ -37,31 +48,40 @@ function App() {
                 clearInterval(intervalId1.current);
                 setSecond(0);
                 setMinute(0);
-                playAlarm();
+                audio.play().then();
                 setFinished(true);
+                setBreakTitle(breakTimeTitle)
             }
         }
-    }, [minute, second, isOn])
+    }, [minute, second, isOn, audio])
 
     const start = () => {
         setIsOn(true);
+        setBreakTitle(workingTimeTitle);
     }
 
     const reset = () => {
-        audio = null;
+        audio.pause();
+        setAudio(new Audio('/DiVeNha.m4a'));
         setIsOn(false);
-        setMinute(1);
-        setSecond(60);
+        setMinute(25);
+        setSecond(0);
         setFinished(false);
+        setBreakTitle('');
+    }
+
+    const pause = () => {
+        clearInterval(intervalId1.current);
+        setIsOn(false);
+        setBreakTitle('PAUSE');
     }
 
     return (
         <div className="App">
-            <button onClick={start}>Start</button>
+            <h1 style={{height: 40}}>{breakTitle}</h1>
+            <button onClick={isOn ? pause : start} disabled={finished}>{isOn ? 'Pause' : 'Start'}</button>
             {finished ? <button onClick={reset}>Reset</button> : null}
-            {isOn ? <h2>{minute < 10 ? `0${minute}` : minute} : {second < 10 ? `0${second}` : second}</h2> : null}
-
-
+            <h2>{minute < 10 ? `0${minute}` : minute} : {second < 10 ? `0${second}` : second}</h2>
         </div>
     );
 }
